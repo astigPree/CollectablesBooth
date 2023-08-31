2e2dc1ae182b7d35a26a977e6cf7b6be7bb21a4a
astigPree <121760681+astigPree@users.noreply.github.com>
2023-08-31 17:15:45 +0800

    Add files via upload

---
+++ data_manager.py
@@ -0,0 +1,137 @@
+import json
+import typing as tp
+import os
+from uuid import uuid4
+import random
+
+"""
+    Card Information:
+        - rarity ; ( SS, S+, S , A+ , A , B+ , B , C )
+        - picture ; file location
+        - quote ; 
+        - value ; card number value
+    
+    Collectables Data Structure:
+        HIGH : SS, S+, S
+        MID : A+ , A , B+
+        LOW : B , C
+        
+        { 
+            'high' : [ ( rarity, picture, quote, value ), .. ],
+            'mid' : [ ( rarity, picture, quote, value ), .. ],
+            'low' : [ ( rarity, picture, quote, value ), .. ]
+        }
+    
+    Collections Data Structure:
+        {
+            'SS' : [ ( rarity, picture, quote, value ), ... ],
+            'S+' : [ ( rarity, picture, quote, value ), ... ],
+            'S' : [ ( rarity, picture, quote, value ), ... ],
+            'A+' : [ ( rarity, picture, quote, value ), ... ],
+            'A' : [ ( rarity, picture, quote, value ), ... ],
+            'B+' : [ ( rarity, picture, quote, value ), ... ],
+            'B' : [ ( rarity, picture, quote, value ), ... ],
+            'C' : [ ( rarity, picture, quote, value ), ... ],
+        }
+    
+    User Information Data Structure:
+        {
+            username : str,
+        }
+    
+"""
+COLLECTION_FILE = "collection.json"
+COLLECTABLES_FILE = "collectables.json"
+USER_FILE = "user_information.json"
+PICTURE_FOLDER = "pictures"
+
+
+class AppManager :
+    collectables: tp.Dict = None
+    collections: tp.Dict = None
+    user_info : tp.Dict = None
+
+    high_password = "1234567890"
+    mid_password = "1234567890"
+    low_password = "1234567890"
+
+    def loadUserInformation(self):
+        if not os.path.exists(USER_FILE):
+            user_info = { "user_name" : str(uuid4())[:10] }
+            with open(USER_FILE , 'w') as jf:
+                json.dump(user_info , jf)
+
+        with open(USER_FILE , 'r') as jf:
+            self.user_info = json.load(jf)
+
+    def saveUserInformation(self):
+        with open(USER_FILE , "w") as jf:
+            json.dump(self.user_info , jf)
+
+    def loadCollections(self) :
+        with open(COLLECTION_FILE, 'r') as jf :
+            self.collections = json.load(jf)
+
+    def saveCollections(self):
+        with open(COLLECTION_FILE , 'w') as jf:
+            json.dump(self.collections , jf)
+
+    def loadCollectables(self) :
+        if self.collectables is not None: return
+
+        with open(COLLECTABLES_FILE, 'r') as jf :
+            self.collectables = json.load(jf)
+
+    def getTotalNumberOfCards(self) -> int :
+        total = 0
+        for key in self.collections :
+            total += len(self.collections[key])
+        return total
+
+    def getNumberOfRarity(self) -> list[tuple[str, int], ...] :
+        return [(rarity, len(self.collections[rarity])) for rarity in self.collections]
+
+    def getHighCard(self , password : str) -> tp.Union[None , tuple , bool]:
+        if password != self.high_password:
+            return None # its mean incorrect password
+
+        cards = [ card for card in self.collectables['high'] if card not in self.collections['SS'] and card not in self.collections['S+'] and card not in self.collections['S'] ]
+        if not cards:
+            return True # its mean you already get all the card in this tier
+
+        new_card = random.choice(cards)
+        self.collections[new_card[0]].append(new_card)
+        return new_card # its mean you get a new card
+
+    def getMidCard(self, password : str ):
+        if password != self.mid_password:
+            return None # its mean incorrect password
+
+        cards = [ card for card in self.collectables['mid'] if card not in self.collections['A'] and card not in self.collections['A+'] and card not in self.collections['B+'] ]
+        if not cards:
+            return True # its mean you already get all the card in this tier
+
+        new_card = random.choice(cards)
+        self.collections[new_card[0]].append(new_card)
+        return new_card # its mean you get a new card
+
+    def getLowCard(self , password : str):
+        if password != self.low_password:
+            return None # its mean incorrect password
+
+        cards = [ card for card in self.collectables['low'] if card not in self.collections['B'] and card not in self.collections['C']]
+        if not cards:
+            return True # its mean you already get all the card in this tier
+
+        new_card = random.choice(cards)
+        self.collections[new_card[0]].append(new_card)
+        return new_card # its mean you get a new card
+
+
+app = AppManager()
+app.loadCollections()
+app.loadUserInformation()
+app.loadCollectables()
+
+print(app.getTotalNumberOfCards())
+print(app.getHighCard("1234567890"))
